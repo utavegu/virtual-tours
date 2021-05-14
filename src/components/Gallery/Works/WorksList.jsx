e549387c6c900526a2991d5a87b870b0a6782b2d
Utavegu <utavegu@list.ru>
2021-05-14 20:35:34 +0500

    Реализовал динамическую подгрузку контента

--- src/components/Gallery/Works/WorksList.jsx
+++ src/components/Gallery/Works/WorksList.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import s from './WorksList.module.css';
 import WorkItem from './WorkItem'
 
 function WorksList({projects}) {
+  const [displayedProjects, setDisplayedProjects] = useState(6);
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler)
+    return () => {
+      document.removeEventListener('scroll', scrollHandler)
+    }
+  }, [displayedProjects])
+
+  const shownProjects = projects.slice(0, displayedProjects);
+
+  const scrollHandler = ({target}) => {
+    if (
+      target.documentElement.scrollHeight - (target.documentElement.scrollTop + window.innerHeight) < 100
+      &&
+      displayedProjects <= projects.length
+    ) setDisplayedProjects(displayedProjects + 3);
+    if (displayedProjects >= projects.length) document.removeEventListener('scroll', scrollHandler);
+  }
+
   return (
     <section className={s.works}>
       <h2 className="visually-hidden">Галерея</h2>
       <ul className={s.work_list}>
-        {projects.map(project => <WorkItem project={project} key={project.id} />)}
+        {shownProjects.map(project => <WorkItem project={project} key={project.id} />)}
       </ul>
     </section>
   )
@@ -19,4 +39,3 @@ WorksList.propTypes = {
 }
 
 export default WorksList
-
