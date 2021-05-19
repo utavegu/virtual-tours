b6f7662040cf20e25484ed273721f3552f597425
Utavegu <utavegu@list.ru>
2021-05-19 13:31:12 +0500

    Финальные правки

--- src/components/Work/VirtualTours/Tour.jsx
+++ src/components/Work/VirtualTours/Tour.jsx
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 // import s from './Tour.module.css';
 
 function Tour({tour}) {
-  console.log(tour);
   return (
     <>
       {/* <h2 className={s.heading}>{tour.name}</h2> */}
       <div className="responsive_container">
-        <a href={tour.url}>
-          <img src="" alt={tour.name} />
+        <a href={tour.url} target="_blank" rel="noreferrer" title={`Посмотреть виртуальный тур "${tour.name}"`}>
+          <img src={tour.poster} alt={tour.name} />
         </a>
         {/*
+        Решение, пока заказчик не подключил SSL-сертификат
         <iframe
           width="900"
           height="500"
